b668e73563b67dc01f604267c608d735f39ed248
CWP <77127867+Camwp@users.noreply.github.com>
2024-01-11 14:05:23 -0800

    scripts optimization, portfolio optimization, created new page template

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,9 +1,3 @@
-function toggleNav() {
-    console.log('Hamburger icon clicked!');
-    var nav = document.querySelector('nav');
-    nav.classList.toggle('active');
-}
-
 // Add this to your existing JavaScript code
 let currentImageIndex = 0;
 const totalImages = document.querySelectorAll(".mainimg").length,
@@ -16,28 +10,11 @@ function showSlides() {
                 showSlides();
             }), 200) // Wait for the transition to complete before changing image
         }), 800) // Transition duration
-    }), 6000) // Change image every 2 seconds
+    }), 6000) // Change image every 6 seconds
 }
 
 // Initial setup
 showSlides();
-
-// Shrink the NAV on scroll
-document.addEventListener("DOMContentLoaded", (function() {
-    var e = document.querySelector("header"),
-        t = document.querySelector(".iconlogo");
-    window.addEventListener("scroll", (function() {
-        window.scrollY > 0 ? (e.classList.add("nav-shrink"), t.style.height = "50px") : (e.classList.remove("nav-shrink"), t.style.height = "100px", t.style.transition = "height 0.3s ease-in-out")
-    }))
-}));
-
-// Function to simulate a click on the first category link
-function selectInitialCategory() {
-    const firstCategoryLink = document.querySelector('.portfolio-categories ul li a');
-    if (firstCategoryLink) {
-        firstCategoryLink.click(); // Simulate a click on the first category link
-    }
-}
 
 var i = 0;
 var txt = 'Experience You Can Trust'; /* The text */
@@ -51,4 +28,4 @@ function typeWriter() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', selectInitialCategory), document.addEventListener('DOMContentLoaded', typeWriter);
+document.addEventListener('DOMContentLoaded', typeWriter);
