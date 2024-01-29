35bbbeeb304643642934fa1a0550ff25c231a1af
Camwp <camwpedro@gmail.com>
2024-01-29 14:34:32 -0800

    new-template

--- scripts/common.js
+++ scripts/common.js
@@ -4,14 +4,15 @@ function toggleNav() {
     nav.classList.toggle('active');
 }
 
-// Shrink the NAV on scroll
-document.addEventListener("DOMContentLoaded", (function() {
-    var e = document.querySelector("header"),
-        t = document.querySelector(".iconlogo");
-    window.addEventListener("scroll", (function() {
-        window.scrollY > 0 ? (e.classList.add("nav-shrink"), t.style.height = "50px", t.style.width = "auto") : (e.classList.remove("nav-shrink"), t.style.height = "100px", t.style.transition = "height 0.3s ease-in-out")
-    }))
-}));
+//```// Shrink the NAV on scroll
+//document.addEventListener("DOMContentLoaded", (function() {
+//    var e = document.querySelector("header"),
+//        t = document.querySelector(".iconlogo");
+//    window.addEventListener("scroll", (function() {
+//        window.scrollY > 0 ? (e.classList.add("nav-shrink"), t.style.height = "50px", t.style.width = "auto") : (e.classList.remove("nav-shrink"), t.style.height = "100px", t.style.transition = "height 0.3s ease-in-out")
+//    }))
+//}));
+//```
 
 //Dropdown function
 $(document).ready(function () {
