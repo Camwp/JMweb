b75659661918ace02618867cde50fd0a936186c9
Camwp <camwpedro@gmail.com>
2024-03-11 10:00:43 -0700

    services

--- scripts/services.js
+++ scripts/services.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const services = document.querySelectorAll('.service');
+    const checkboxes = document.querySelectorAll('.toggleCheckbox');
 
-    services.forEach(function(service) {
-        const header = service.querySelector('.header'); // Get the header element within each service
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+            const dropdown = checkbox.parentElement; // Get the parent li element
 
-        console.log(header); // Check if the header is correctly selected
-
-        header.addEventListener('click', function() {
-            console.log("Clicked!"); // Check if the click event is triggered
-
-            service.classList.toggle('active'); // Toggle the 'active' class for the clicked service
+            if (checkbox.checked) {
+                dropdown.style.maxHeight = '75px';
+            } else {
+                dropdown.style.maxHeight = 'none';
+            }
         });
     });
 });
